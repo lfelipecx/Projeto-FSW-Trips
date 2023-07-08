3fbbb6c3075fa8baae81d73a3fc0260d55bd7e0e
Luiz Felipe <lfelipecx@gmail.com>
2023-07-08 17:25:23 -0300

    feat: add my trips button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
 
   return (
-    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center lg:border-b lg:border-grayLighter">
       <Link href="/">
         <div className="relative h-[32px] w-[182px]">
           <Image src="/logo.png" alt="Full Stack Week" fill />
@@ -41,8 +41,12 @@ const Header = () => {
           <Image height={35} width={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
 
           {menuIsOpen && (
-            <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-              <button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
+            <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <Link href="/my-trips" onClick={() => setMenuIsOpen(false)}>
+                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">Minhas Viagens</button>
+              </Link>
+
+              <button className="text-primary pt-2 text-sm font-semibold" onClick={handleLogoutClick}>
                 Logout
               </button>
             </div>
