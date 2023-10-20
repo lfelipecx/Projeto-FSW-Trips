83595ef29d53e6b06c52f3bad2f81682f4babda4
Luiz Felipe <lfelipecx@gmail.com>
2023-10-20 17:14:27 -0300

    feat: remove Toast component in import on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import { Poppins } from 'next/font/google'
 import { NextAuthProvider } from './providers/auth';
+import ToastProvider from './providers/toast';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import ToastProvider from './providers/toast';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "600", "700", "800", "900"] })
 
